db65c179a5402d1de3262077221b5312ae22f13c
mi-24v <yuusukessid@gmail.com>
2016-02-26 17:45:12 +0900

    さんぷるこうしん

---
+++ AmpanManMarch/Assets/Script/CameraBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+	public Transform target;//追尾先オブジェクト
+	private Vector3 offset;//距離
+
+	// Use this for initialization
+	void Start () {
+		offset = GetComponent<Transform> ().position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Transform> ().position = target.position + offset;
+	}
+}
---
+++ AmpanManMarch/Assets/Script/Cube.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+	public float speed = 4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		if (Input.GetKey ("right")) {
+			rigidbody.AddForce (speed, 0, 0);
+		} else if (Input.GetKey ("left")) {
+			rigidbody.AddForce (-speed, 0, 0);
+		} else if (Input.GetKey ("up")) {
+			rigidbody.AddForce (0, 0, speed);
+		} else if (Input.GetKey ("down")) {
+			rigidbody.AddForce(0,0,-speed);
+		}
+	}
+}
