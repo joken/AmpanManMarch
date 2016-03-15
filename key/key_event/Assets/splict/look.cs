e11c3f47d9d618277d7f75a7c6a011450a428dad
雄太 木俣 < @yuta>
2016-03-15 11:46:24 +0900

    readmeを少し改変、新しいプロジェクトの追加（左右＋スペース）

---
+++ key/key_event/Assets/splict/look.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class look : MonoBehaviour {
+	public GameObject ob;
+
+	// Use this for initialization
+	void Start () {
+		ob = GameObject.Find ("Cube");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (ob.transform.position);
+	}
+}
