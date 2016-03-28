cc9a041e4090765ef78cc9f82811a7c91c26e312
雄太 木俣 < @yuta>
2016-03-28 20:41:52 +0900

    key

---
+++ key/key_event/Assets/sclipt/HeroMan.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMan : MonoBehaviour {
+	public Vector3 dir = new Vector3(Mathf.Sqrt(2.0f),0,Mathf.Sqrt(2.0f));
+	public Vector3 pos;
+	public Vector3 posMan;
+
+	public float ang  = 0.0f;
+	public float speedMan = 10.0f;
+	public bool col = false;
+	public GameObject camera;
+	public GameObject camMan;
+
+	// Use this for initialization
+	void Start () {
+		pos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//pos = gameObject.transform.position;
+		/*if (Input.GetKey (KeyCode.UpArrow) && !(col)) {
+			pos.x -= ((pos - camera.transform.position) * speedMan).x;
+			pos.z -= ((pos - camera.transform.position) * speedMan).z;
+		} else if (Input.GetKey (KeyCode.RightArrow) && !(col)) {
+			posMan.x = ((pos - camera.transform.position) * speedMan).z;
+			posMan.z = ((pos - camera.transform.position) * speedMan).x;
+			posMan.y = 0;
+			pos += posMan;
+		} else if (Input.GetKey (KeyCode.LeftArrow) && !(col)) {
+			posMan.x = -((pos - camera.transform.position) * speedMan).z;
+			posMan.z = -((pos - camera.transform.position) * speedMan).x;
+			posMan.y = 0;
+			pos += posMan;
+
+		} else if (Input.GetKey (KeyCode.DownArrow) && !(col)) {
+			pos.x += ((pos - camera.transform.position) * speedMan).x;
+			pos.z += ((pos - camera.transform.position) * speedMan).z;
+		}*/
+
+
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			this.GetComponent<Rigidbody>().AddForce(
+				dir,ForceMode.VelocityChange);
+		} if (Input.GetKey(KeyCode.RightArrow)) {
+			ang += 0.05f;
+			dir.x = Mathf.Sin (ang);
+			dir.z = Mathf.Cos (ang);
+		}
+			 if (Input.GetKey(KeyCode.LeftArrow)) {
+			ang -= 0.05f;
+			dir.x = Mathf.Sin (ang);
+			dir.z = Mathf.Cos (ang);
+		}
+		 if (Input.GetKey(KeyCode.DownArrow)) {
+			this.GetComponent<Rigidbody>().AddForce(
+				-dir,ForceMode.VelocityChange);
+		}
+		//camera.transform.position = gameObject.transform.position-(pos - gameObject.transform.position) * 1000.0f;
+		//camera.transform.LookAt (gameObject.transform.position);
+		//Debug.Log (ang);
+		//Debug.Log (Mathf.PI);
+		//posMan = gameObject.transform.position - pos;
+		//Debug.Log(Mathf.Acos(posMan.z/Mathf.Sqrt(posMan.x*posMan.x+posMan.z*posMan.z))*180/Mathf.PI);
+
+
+		//transform.rotation = Quaternion.Euler (0, Mathf.Acos(dir.z/Mathf.Sqrt(dir.x*dir.x+dir.z*dir.z))*180/Mathf.PI, 0);
+		//transform.rotation = Quaternion.Euler (0, Mathf.Acos(posMan.z/Mathf.Sqrt(posMan.x*posMan.x+posMan.z*posMan.z))*180/Mathf.PI, 0);
+		CamMan();
+		//gameObject.transform.position = pos;
+
+
+	}
+	public void CamMan(){
+		camMan.transform.position = gameObject.transform.position;
+		camMan.transform.LookAt (gameObject.transform.position+dir);
+	}
+}
---
+++ key/key_event/Assets/sclipt/goal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goal: MonoBehaviour {
+	public GameObject planet;
+	public GameObject cube;
+	Vector3 m_pos;
+	// Use this for initialization
+	void Start () {
+		//Emp = GameObject.Find ("Emp") as GameObject;
+		m_pos = transform.position;
+
+
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = cube.transform.position;
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			for (int i = 1; i < 10; i++) {
+				m_pos.x = m_pos.x + 5 * Mathf.Cos (0.7f * i);
+				m_pos.z = m_pos.z + 5 * Mathf.Sin (0.7f * i);
+
+				GameObject p = (GameObject)Instantiate (planet, m_pos, Quaternion.identity);
+				p.transform.parent = gameObject.transform;
+				m_pos = transform.position;
+			}
+		}
+		
+	}
+}
