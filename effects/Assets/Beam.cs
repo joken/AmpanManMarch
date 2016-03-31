182a72f63251e797df12b53375be8d0b20ea5797
雄太 木俣 < @yuta>
2016-03-31 22:19:27 +0900

    beam

---
+++ effects/Assets/Beam.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beam : MonoBehaviour {
+	public GameObject b1;
+	public GameObject b2;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		b1.transform.LookAt (b1.transform.position-(b2.transform.position-b1.transform.position));
+	}
+}
