8fea3fd77390b976993a5710d59e57c2b72e9540
雄太 木俣 < @yuta>
2016-03-31 19:19:50 +0900

    ばちばち

---
+++ effects/Assets/bachi1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bachi1 : MonoBehaviour {
+	public ParticleSystem bachi;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)){
+			//Debug.Log ("test");
+			bachi.transform.position = new Vector3 (Random.Range (-2.0f, 2.0f), Random.Range (-2.0f, 2.0f), Random.Range (-2.0f, 2.0f));
+			bachi.Play ();
+		}
+	
+	}
+}
