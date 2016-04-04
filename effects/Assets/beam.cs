7d252a935250f2a133bac499a8b7820b25a8931a
kantentenka <yutakimata@gmail.com>
2016-04-04 21:11:16 +0900

    くっつけた

---
+++ effects/Assets/beam.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beam : MonoBehaviour {
+	public ParticleSystem[] beams1 = new ParticleSystem[3];
+	public ParticleSystem[] beams2 = new ParticleSystem[3];
+	public ParticleSystem[] beamb = new ParticleSystem[4];
+	public GameObject hand1;
+	public GameObject hand2;
+	public GameObject elbow1;
+	public GameObject elbow2;
+	public GameObject heart;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (effectMan.beams) {
+			if (!effectMan.col) {
+				player (beams1);
+				player (beams2);
+				stopper (beamb);
+				hand1.transform.LookAt (hand1.transform.position - (elbow1.transform.position - hand1.transform.position));
+				hand2.transform.LookAt (hand2.transform.position - (elbow2.transform.position - hand2.transform.position));
+			} else {
+				stopper (beams1);
+				stopper (beams2);
+				player (beamb);
+				hand1.transform.LookAt (hand1.transform.position - (heart.transform.position - hand1.transform.position));
+			}
+		} else {
+			stopper (beams1);
+			stopper (beams2);
+			stopper (beamb);
+		}
+
+
+	}
+	void player( ParticleSystem[] p){
+		foreach (ParticleSystem i in p) {
+			i.Play ();
+		}
+	}
+	void stopper( ParticleSystem[] p){
+		foreach (ParticleSystem i in p) {
+			i.Stop ();
+		}
+	}
+}
---
+++ effects/Assets/byfoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class byfoot : MonoBehaviour {
+	public GameObject foot1;
+	public GameObject foot2;
+	public ParticleSystem un;
+	public Color unC = new Color (0,0,0,1);
+	public Vector3 dis;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		un.startColor = unC;
+		unC.a = 1;
+		unC.r = 1-foot1.transform.position.y * 0.1f;
+		unC.g = 1-foot2.transform.position.y * 0.1f;
+		dis = foot1.transform.position - foot2.transform.position;
+		unC.b = (Mathf.Sqrt (dis.x * dis.x + dis.z * dis.z) - 2) * 0.1f > 0 ? 1-(Mathf.Sqrt (dis.x * dis.x + dis.z * dis.z) - 6) * 0.1f:1 ;
+		Debug.Log (un.startColor);
+	}
+}
