41135f93fd518038b1b98d5c5d4d891b41ad33ff
kantentenka <yutakimata@gmail.com>
2016-04-04 16:33:06 +0900

    水完成

--- effects/Assets/water.cs
+++ effects/Assets/water.cs
@@ -1,32 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine;
 public class water : MonoBehaviour {
-	public float posy;
+	public float posy=0;
 	public bool cl = true;
 
 	public GameObject wat;
 	public GameObject wav;
+	public Object cwat;
+	public Object cwav;
 	// Use this for initialization
 	void Start () {
-		wav.transform.parent = gameObject.transform;
+		//wav.transform.position = gameObject.transform;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.Space)) {
 			if (cl) {
 				cl = false;
-				Instantiate (wat, gameObject.transform.position, Quaternion.identity);
-				Instantiate (wav, new Vector3 (0, posy, 0), Quaternion.identity);
-			} else {
-			//	wav.transform.position  = new Vector3(0,wav.transform.position.y +0.5f,0);
-			}
+				cwat = Instantiate (wat, gameObject.transform.position, Quaternion.identity);
+				cwav = Instantiate (wav, new Vector3 (0, posy, 0), Quaternion.identity);
+			} 
+
 		}else{
-			cl = true;
-			GameObject.Destroy(wav);
-			GameObject.Destroy(wat);
+			
+			if (!cl) {
+				//GameObject.Destroy (wav);
+				UnityEngine.Object.Destroy (cwat);
+				UnityEngine.Object.Destroy (cwav);
 			}
+			cl = true;
+			//Object.Destroy (wat);
+
+
+		}
 				
 	}
 }
